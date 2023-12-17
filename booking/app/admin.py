255ebf7163b2b018692bd8967f6eb1799b0cdba1
kirakirag <50074561+kirakirag@users.noreply.github.com>
2023-12-17 15:16:40 +0300

    Register Room and Booking models in admin panel

--- booking/app/admin.py
+++ booking/app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from app.models import Room, Booking
 
-# Register your models here.
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price_per_night', 'capacity')
+    search_fields = ('name',)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('booking_number', 'user', 'room',
+                    'start_date', 'end_date', 'status')
+    search_fields = ('booking_number', 'user__username', 'room__name')
+    list_filter = ('status',)
+    readonly_fields = ('booking_number',)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('user', 'room')
+        return self.readonly_fields
